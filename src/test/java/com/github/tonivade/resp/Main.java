ebfa75299d4c4a14d3e1e1923a3de26858a96363
Antonio Muñoz <antoniogmc@gmail.com>
2026-03-23 21:43:46 +0100

    set root log level to info

--- src/test/java/com/github/tonivade/resp/Main.java
+++ src/test/java/com/github/tonivade/resp/Main.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright (c) 2015-2026, Antonio Gabriel Muñoz Conejo <me at tonivade dot es>
+ * Distributed under the terms of the MIT License
+ */
 package com.github.tonivade.resp;
+
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
 
 public class Main {
 
   public static void main(String[] args) {
+    setRootLogLevel(Level.INFO);
     RespServer.builder().build().start();
   }
 
+  private static void setRootLogLevel(Level level) {
+    LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+    Logger rootLogger = context.getLogger(Logger.ROOT_LOGGER_NAME);
+    rootLogger.setLevel(level);
+  }
 }
